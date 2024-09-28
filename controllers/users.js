a50ccd1d2acf390fe1dfae575733530dbc35cc05
creestian <creestian@gmail.com>
2024-09-28 20:19:02 -0300

    update Week 2

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const { createSecureServer } = require('http2');
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
@@ -39,8 +40,97 @@ const getAll = async (req, res, next) => {
   }
   };
 
+  //------------------------------------
+  // CREATE USER
+  //------------------------------------
+  
+  const createUser = async (req, res, next) => {
+    try{
+      const user = {
+        name: req.body.firstName,
+        name: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+      };
+      const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('users')
+        .insertOne(user);
+        if (response.acknowledged && response.insertedId) {
+          res.status(201).json({ id: response.insertedId });
+        } else {
+          res.status(500).json({ message: 'Failed to create user' });
+        }
+    }catch(err) {
+      res.status(500).json(err);
+  }
+  };
+
+  //------------------------------------
+  // UPDATE USER
+  //------------------------------------
+  
+  const updateUser = async (req, res, next) => {
+    try{
+      const userId = new ObjectId(req.params.id);
+      const user = {
+        name: req.body.firstName,
+        name: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+      };
+      const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('users')
+        .replaceOne({ _id: userId, user});
+        if (response.modifiedcount > 0 && response.insertedId) {
+          res.status(201).json({ id: response.insertedId });
+        } else {
+          res.status(500).json({ message: 'Failed to update user' });
+        }
+    }catch(err) {
+      res.status(500).json(err);
+  }
+  };
+
+  //------------------------------------
+  // DELETE USER
+  //------------------------------------
+  
+  const deleteUser = async (req, res, next) => {
+    try{
+      const userId = new ObjectId(req.params.id);
+      const user = {
+        name: req.body.firstName,
+        name: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+      };
+      const result = await mongodb
+        .getDatabase()
+        .db()
+        .collection('users')
+        .remove({ _id: userId, user});
+        if (response.modifiedcount > 0 && response.insertedId) {
+          res.status(201).json({ id: response.insertedId });
+        } else {
+          res.status(500).json({ message: 'Failed to delete user' });
+        }
+    }catch(err) {
+      res.status(500).json(err);
+  }
+  };
+
 
   module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
   }
--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,11 @@ const usersController = require('../controllers/users');
 router.get('/', usersController.getAll);
 //Get ony document.
 router.get('/:id', usersController.getSingle);
+//Get all the collection.
+router.post('/', usersController.createUser);
+//Get ony document.
+router.put('/:id', usersController.updateUser);
+//Get ony document.
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
